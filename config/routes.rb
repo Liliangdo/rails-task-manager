e0533d171f56458758b4ea90797f3594e197384f
lilian garrido <lilian.garrido13@gmail.com>
2016-08-09 12:42:40 +0200

    edit corretly my routes,set index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
-  get '/tasks/:id/show', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   get '/tasks/new', to: 'tasks#new'
   post '/tasks/create', to: 'tasks#create'
-  get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id/update', to: 'tasks#update'
-  delete '/tasks/:id/delete', to: 'tasks#delete'
+  delete '/tasks/:id', to: 'tasks#delete'
 end
